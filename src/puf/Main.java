5bd18403e875a161a3ec2a4c958388fa23fa55ce
perkles <tarcisio.neto@boavistatecnologia.com.br>
2022-08-16 10:39:33 -0300

    classe luta

--- src/puf/Main.java
+++ src/puf/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Lutador l[] = new Lutador[8];
 
-        l[0] = new Lutador("Lucas Cavalcante(Papa Tango)", "Good Garden", 24, 1.75f, 68.9f, 11, 2, 1);
+        l[0] = new Lutador("Lucas Cavalcante(Papa Tango)", "Good Garden", 24, 1.84f, 83.0f, 11, 2, 1);
         l[1] = new Lutador("Colares", "Cid. dos Funcionarios", 32, 1.68f, 57.8f, 14, 2, 3);
         l[2] = new Lutador("Tarcísio Neto", "Maraponga", 35, 1.71f, 78.3f, 12, 2, 1);
         l[3] = new Lutador("Felipe Alexandre", "Parquelândia", 23, 1.93f, 81.6f, 13, 0, 2);
@@ -13,8 +13,8 @@ public class Main {
         l[6] = new Lutador("Isadora Rocha(India)", "Caucaia", 20, 1.60f, 55.7f, 15, 4, 4);
         l[7] = new Lutador("Rhayane(Rhayagil)", "Meirelles", 24, 1.58f, 58.4f, 20, 2, 4);
 
-        l[0].apresentar();
-        l[0].status();
-
+        Luta PUF01 = new Luta();
+        PUF01.marcarLuta(l[0], l[5]);
+        PUF01.lutar();
     }
 }
